29cefb23fbd5d87028d6ac4abef8761b8f8196f2
LAujust <61076300+LAujust@users.noreply.github.com>
2026-07-01 14:30:48 +0000

    Deploying to gh-pages from @ LAujust/laujust.github.io@0dfd5f04420b41b85faa371663777c7e93bc3462 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -96,7 +96,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%75%6A%75%73%74@%6D%61%69%6C.%75%73%74%63.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -104,6 +104,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-6223-840X", "_blank");
         },
       },{
         id: 'social-rss',
