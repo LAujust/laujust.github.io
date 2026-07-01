b3f32ac2810609e13300552329ba3983372853b3
LAujust <61076300+LAujust@users.noreply.github.com>
2026-07-01 14:04:58 +0000

    Deploying to gh-pages from @ LAujust/laujust.github.io@be635763b75196d746188779921d7463b7dde7c9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -83,10 +83,7 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-the-homepage-is-finally-under-construction",
+            },},{id: "news-the-homepage-is-finally-under-construction",
           title: 'The homepage is finally under construction.',
           description: "",
           section: "News",},{id: "projects-project-1",
